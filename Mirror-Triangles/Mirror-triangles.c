8e42fb9974ebfee8741c3c50c8623a6aff940504
Durgesh-babu <durgeshbabu.girajala@gmail.com>
2021-10-01 03:05:17 -0400

    Add Mirror triangle project.
    Prints two triangles facing each other like mirror reflection

---
+++ Mirror-Triangles/Mirror-triangles.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    int n;                            //Variable contains height and width of the triangle
+    int a=0;                          //Variable  used to specify #'s to fill the triangle in particular row
+    do
+    {
+        n = get_int("Height: ");      //Takes input from the user
+    }
+    while (n<=0 || n>=20);            //Loop limits the user to choose the height between [0,20]
+    
+    for (int i=0; i<n; i++)           //Loop runs the no of rows
+    {
+        a++;                          ////No of the hashes to be filled
+        for (int j=0; j<n-a; j++)     //Fills 'n-a' number of spaces in the respective row
+        {
+            printf(" ");
+        }
+
+        for (int k=0; k<a; k++)       //Fills 'a' number of #'s after spaces in the respective row
+        {
+            printf("#");
+        }
+
+        printf("  ");                 //Fills the gap between the two triangles
+
+        for (int l=0; l<a;l++)        //Loop creates a general left sided triangle with '#'s
+        {
+            printf("#");
+        }
+
+        printf("\n");                 //Changes row after filling it with 'n-a' no. of '#'s and 'a' no. of ' 's 
+    }
+}
+
