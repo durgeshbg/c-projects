65f6851501e572643e5124820226e301b33a7a0c
Durgesh-babu <durgeshbabu.girajala@gmail.com>
2021-10-04 00:55:58 -0400

    Add Swaping project.
    Program swaps two numbers using a third variable.

---
+++ Swaping/swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main (void)
+{
+    int a =  get_int("First number: ");                               //First number
+    int b = get_int("Second number: ");                               //Second number
+    int temp;                                                         //Temorary variable
+    
+    temp = a;                                                         //Swaping the two number
+    a = b;
+    b = temp;
+
+    printf("First number (new): %i\nSecond number (new): %i", a, b); //Printing the swapped values
+}
